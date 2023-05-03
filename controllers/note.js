4ef0b573ba89d2c6a5aa76ba8bf3e7fb1eb11352
petrKantek <57535586+petrKantek@users.noreply.github.com>
2023-05-03 13:09:16 +0200

    change to azure cosmos db

--- controllers/note.js
+++ controllers/note.js
@@ -1,46 +1,56 @@
+const Note = require("../models/noteModel");
 const CustomError = require("../models/CustomError");
-const db = require("../database/dbConfig");
 
 exports.getNotes = async (req, res, next) => {
   try {
-    const notes = await db.any('select * from main.notes');
+    const notes = await Note.find();
     res.status(200).json({ notes });
   } catch (error) {
     next(new CustomError(error.message, 500));
   }
-}
+};
 
-exports.addNote = async (req, res, next) => {
-  try {
-    const { name, description, status } = req.body;
-    const result = await db.one('insert into main.notes(name, description, status) values($1, $2, $3) returning *', [name, description, status]);
-    const notes = await db.any('select * from main.notes');
-    res.status(201).json({ message: 'Note added successfully', note: result, notes: notes });
-  } catch (error) {
-    next(new CustomError(error.message, 500));
-  }
-}
+export async function addNote(req, res, next) {
+    try {
+      const body = req.body;
+      const note = new Note({
+        name: body.name,
+        description: body.description,
+        status: body.status,
+      });
+      const newNote = await note.save();
+      const allNotes = await find();
+      res
+        .status(201)
+        .json({ message: "Note added", note: newNote, notes: allNotes });
+    } catch (error) {
+      next(new CustomError(error.message, 500));
+    }
+  };
 
 exports.deleteNote = async (req, res, next) => {
   try {
-    const noteId = req.params.id;
-    const result = await db.oneOrNone('delete from main.notes where id = $1 returning *', noteId);
-    if (!result) {
+    const deletedNote = await Note.findByIdAndRemove(req.params.id);
+    if (deletedNote == null) {
       return res.status(404).json({
         message: "Note not found",
       });
     }
-    const notes = await db.any('select * from main.notes');
-    res.status(200).json({ message: 'Note deleted successfully', note: result, notes: notes });
+    const allNotes = await Note.find();
+    res.status(200).json({
+      message: "Note deleted",
+      note: deletedNote,
+      notes: allNotes,
+    });
   } catch (error) {
-    next(new CustomError(error.message, error.status || 500));
+    next(new CustomError(error.message, 500));
   }
-}
+};
 
 exports.numNotes = async (req, res, next) => {
   try {
-    const result = await db.one('select count(*) from main.notes');
-    res.status(200).json({ message: 'Number of notes retrieved successfully', count: result.count });
+    const count = await Note.countDocuments();
+    res.status(200).json({ message: 'Number of notes retrieved successfully', count });
   } catch (error) {
     next(new CustomError(error.message, 500));
   }
--- database/dbConfig.js
+++ database/dbConfig.js
@@ -1,6 +1,20 @@
-const pgp = require('pg-promise')(/* options */)
-// change the connection string below to Azure App Service connection string from env variables
+const mongoose = require("mongoose");
+require("dotenv").config();
 
-const db = pgp(`postgres://${process.env.AZURE_POSTGRESQL_USER}:${process.env.AZURE_POSTGRESQL_PASSWORD}@${process.env.AZURE_POSTGRESQL_HOST}:${process.env.AZURE_POSTGRESQL_PORT}/${process.env.AZURE_POSTGRESQL_DATABASE}?ssl=${process.env.AZURE_POSTGRESQL_SSL}`)
+const connOptions = {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+};
 
-module.exports = db;
+const MONGO_URI = process.env.AZURE_COSMOS_CONNECTIONSTRING;
+
+const connectToDB = async () => {
+  try {
+    const connect = await mongoose.connect(MONGO_URI, connOptions);
+    if (connect) console.log(`Mongodb connected - ${connect.connection.host}`);
+  } catch (err) {
+    console.log(`Database error ${err}`);
+  }
+};
+
+module.exports = connectToDB;
